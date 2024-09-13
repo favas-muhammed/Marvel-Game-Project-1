4713890c2b8ce3dcab7887fb17037425a2b450f6
Favas Muhammed <favassakkeer13@gmail.com>
2024-09-13 02:03:01 +0200

    secound commit- final product

--- js/game.js
+++ js/game.js
@@ -9,10 +9,34 @@ class Game {
       this.isGameOver = false;
       this.score = 0;
       this.brightened = [false, false, false, false, false, false];
+      
     }
   
     start() {
-      this.gameLoop();
+      this.showCountdown(() => {
+        this.gameLoop();
+      });
+    }
+  
+    showCountdown(callback) {
+      const countdownElement = document.createElement('div');
+      countdownElement.id = 'countdown';
+      document.body.appendChild(countdownElement);
+  
+      let countdown = 3;
+      const intervalId = setInterval(() => {
+        countdownElement.innerText = countdown;
+        countdown -= 1;
+  
+        if (countdown < 0) {
+          clearInterval(intervalId);
+          countdownElement.style.display = 'none';
+          document.body.removeChild(countdownElement);
+          callback(); // Start the game
+        }
+      }, 1000);
+  
+      countdownElement.style.display = 'block';
     }
   
     gameLoop() {
@@ -68,6 +92,10 @@ class Game {
       document.getElementById('points').innerText = this.score;
       
       if (this.score % 10 === 0) {
+
+        const level = this.score / 10;
+        this.showLevelCompletion(level);
+
         const imageIndex = Math.min(Math.floor(this.score / 10) - 1, 5);
         if (!this.brightened[imageIndex]) {
           this.brightenImage(imageIndex);
@@ -75,7 +103,24 @@ class Game {
         }
       }
     }
-  
+
+    showLevelCompletion(level) {
+      let levelCompletionElement = document.getElementById('level-completion');
+      if (!levelCompletionElement) {
+        levelCompletionElement = document.createElement('div');
+        levelCompletionElement.id = 'level-completion';
+        document.body.appendChild(levelCompletionElement);
+      }
+    
+      levelCompletionElement.innerHTML = `Level ${level} Completed`;
+      levelCompletionElement.style.display = 'block';
+    
+      setTimeout(() => {
+        levelCompletionElement.style.display = 'none';
+      }, 3000);
+    }
+
+
     brightenImage(index) {
       const images = document.querySelectorAll('.equal-image');
       if (images[index]) {
@@ -85,7 +130,14 @@ class Game {
   
     gameOver() {
       this.isGameOver = true;
-      alert("Game Over!");
-      window.location.reload();
+      const gameOverElement = document.createElement('div');
+      gameOverElement.id = 'game-over';
+      gameOverElement.innerHTML = '<p>GAME OVER</p><p>Try Again!</p>';
+    
+      document.body.appendChild(gameOverElement);
+      
+      setTimeout(() => {
+        window.location.reload();
+      }, 5000);
     }
   }
